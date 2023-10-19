a549a642c851734cd21d91ee321efbffc868ed9e
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-10-19 10:07:41 -0500

    Time: 1 ms (28.54%) | Memory: 39.4 MB (98.56%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,20 +14,26 @@
  * }
  */
 class Solution {
-    int result;
+    int result = 0;
+    int currNum = 0;
     public int sumNumbers(TreeNode root) {
-        helper(root, 0);
-        return result;
-    }
-
-    private void helper(TreeNode root, int currNum){
-        if(root == null) return;
-        
-        currNum = currNum * 10 + root.val;
-        if(root.left == null && root.right == null){
-            result += currNum;
+        Stack<TreeNode> st = new Stack<>();
+        Stack<Integer> numSt = new Stack<>();
+        while(root !=null || !st.isEmpty()){
+            while(root!=null){
+                st.push(root);
+                currNum = currNum * 10 + root.val;
+                numSt.push(currNum);
+                root = root.left;
+            }
+            root = st.pop();
+            currNum = numSt.pop();
+            if(root.left == null && root.right == null){
+                result += currNum;
+            }
+            root = root.right;
         }
-        helper(root.left, currNum);
-        helper(root.right, currNum);
+        return result;
+
     }
 }
