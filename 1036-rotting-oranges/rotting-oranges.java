17df78b05649e2a1edc1bb85d6d67fa6c91fe12d
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-10-29 19:44:17 -0500

    Time: 1 ms (100.00%) | Memory: 40.7 MB (95.66%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,40 +1,42 @@
 class Solution {
+    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+    int m;
+    int n;
     public int orangesRotting(int[][] grid) {
         Queue<int []> q = new LinkedList<>();
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
+        m = grid.length;
+        n = grid[0].length;
+
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 2){
-                    q.add(new int[]{i, j});
-                }else if(grid[i][j] == 1){
-                    count++;
+                    dfs(grid, i, j, 2);
+                }
+
+            }
+        }
+
+        
+        int time = 2;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1) return -1;
+                else if(grid[i][j] > 2){
+                    time = Math.max(time, grid[i][j]);
                 }
             }
         }
-        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
-        int time = 0;
-        if(count == 0) return time;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                int[] curr = q.poll();
-                for(int[] dir : dirs){
-                    int nr = dir[0] + curr[0];
-                    int nc = dir[1] + curr[1];
-                    if(nr >= 0 && nr<m && nc >= 0 && nc< n && grid[nr][nc] == 1){
-                        q.add(new int[]{nr, nc});
-                        grid[nr][nc]=2;
-                        count--;
-                    }
-                }
-            }
-            time++;
+        if(time == 2) return 0;
+        return time - 2;
+    }
+    private void dfs(int [][] grid, int r, int c, int time){
+        if(r < 0 || r == m || c< 0 || c==n ) return;
+        if(grid[r][c] != 1 && grid[r][c] < time) return;
+        grid[r][c] = time;
+        for(int[] dir: dirs){
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            dfs(grid, nr, nc, time+1);
         }
-        if(count == 0){
-            return time - 1;
-        }
-        return -1;
     }
 }
