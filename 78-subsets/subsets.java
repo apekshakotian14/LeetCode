567812bf984d98ab07d6d3050bbb85cbcc565bd2
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-11-01 12:34:50 -0500

    Time: 1 ms (70.98%) | Memory: 41.5 MB (78.78%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+        this.result = new ArrayList<>();
+        helper(nums, 0, new ArrayList<Integer>());
+        return result;
+    }
+
+    private void helper(int[] nums, int index, List<Integer> path){
+        if(index == nums.length){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        helper(nums, index+1, path);
+        path.add(nums[index]);
+        helper(nums, index+1, path);
+        path.remove(path.size() - 1);
+    }
+}
