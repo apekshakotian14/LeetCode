e7b427059838948aafd9cf34d3afc012e9172bce
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-11-09 16:44:56 -0600

    Time: 0 ms (100.00%) | Memory: 40.2 MB (88.71%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,15 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n==0) return 1.0;
-        double re = myPow(x, n/2);
-        if(n%2 == 0){
-            return re*re;
-        }else{
-            if(n>0){
-                return re*re*x;
-            }else{
-                return re*re*(1/x);
-            }
+        if(n == Integer.MIN_VALUE){
+            x = x*x;
+            n = n/2;
         }
+        if(n<0){
+            x = 1/x;
+            n = n*(-1);
+        }
+        double result = 1.0;
+        while(n>0){
+            if(n%2 != 0){
+                result = result * x;
+            }
+            x = x * x;
+            n = n/2;
+        }
+        return result;
     }
 }
