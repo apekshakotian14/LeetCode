7c718b6f6ffd0925f696964407e7028371da97df
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-11-19 12:29:06 -0600

    Time: 1 ms (21.59%) | Memory: 40.5 MB (88.90%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -30,7 +30,7 @@ class Solution {
     }
     
     public TreeNode retriveFromList(){
-        for(int i=0;i<list.size()-1;++i){
+        for(int i=0;i<list.size()-1;i++){
             TreeNode ele =list.get(i);
             ele.left=null;
             ele.right=list.get(i+1);
