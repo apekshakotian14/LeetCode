0b9e9e73326885be4b3e0098513dfa4ce6fa1a4a
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-11-22 15:34:29 -0600

    Time: 7 ms (51.64%) | Memory: 43.9 MB (11.87%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,29 +1,18 @@
 class Solution {
-    HashMap<String, Boolean> map;
+    
     public boolean wordBreak(String s, List<String> wordDict) {
-        this.map = new HashMap<>();
+        int n = s.length();
         HashSet<String> set = new HashSet<>(wordDict);
-        return helper(s, set);
-
-    }
-
-    private boolean helper(String s, HashSet<String> set){
-        if(s.length() == 0) return true;
-
-        for(int i=0; i<s.length(); i++){
-            String sub = s.substring(0, i+1);
-                if(set.contains(sub)){
-                    String subst = s.substring(i+1);
-                        if(!map.containsKey(subst)){
-                            boolean re = helper(subst, set);
-                            map.put(subst, re);
-                        }
-                        if(map.get(subst)){
-                            return map.get(subst);
-                        }
-                        
-                    }
+        boolean [] dp = new boolean[n+1];
+        dp[0] = true;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                if(dp[j] && set.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
                 }
-                return false;
             }
         }
+        return dp[n];
+    }       
+ }
