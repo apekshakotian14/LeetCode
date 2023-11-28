7a261cb7bd34e2636053cb61ea850e55f87be725
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-11-28 16:49:04 -0600

    Time: 0 ms (100.00%) | Memory: 40.2 MB (75.42%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) return 0;
+        int n = citations.length;
+        int [] buckets = new int[n+1];
+        for(int i=0; i<n ; i++){
+            if(citations[i] >= n){
+                buckets[n]++;
+            }else{
+                buckets[citations[i]]++;
+            }
+        }
+        int sum = 0;
+        for(int i=n; i>=0; i--){
+            sum += buckets[i];
+            if(sum >= i){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
