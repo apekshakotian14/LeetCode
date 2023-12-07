29a7211b83625ab65cc2cb2172debde292fffd24
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2023-12-06 20:18:00 -0600

    Time: 1 ms (99.98%) | Memory: 44.6 MB (30.08%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPosition = nums.length - 1;
+        for(int i = nums.length - 1; i >= 0 ; i--){
+            if(i + nums[i] >= lastPosition) {
+                lastPosition = i;
+            }
+        }
+        return lastPosition == 0;
+    }
+}
