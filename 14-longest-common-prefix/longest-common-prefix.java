a5bf8f5077791f645989477f4b815b7903127c79
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2024-03-06 13:40:43 -0600

    Time: 1 ms (78.09%) | Memory: 41.2 MB (78.51%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,15 +1,29 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0 || strs == null){
-            return null;
+        if(strs == null || strs.length == 0){
+            return "";
         }
-
-        String prefix = strs[0];
-        for(int i=1; i<strs.length; i++){
-            while(strs[i].indexOf(prefix) != 0){
-                prefix = prefix.substring(0, prefix.length() - 1);
-            }
-        }
-        return prefix;
+        return longestCommonPrefix(strs, 0, strs.length - 1);
     }
+
+     public String longestCommonPrefix(String[] strs, int l, int r){
+         if(l==r){
+             return strs[l];
+         }else{
+             int mid = (l+r)/2;
+             String lcpLeft = longestCommonPrefix(strs, l, mid);
+             String lcpRight = longestCommonPrefix(strs, mid+1,r);
+             return commonPrefix(lcpLeft, lcpRight);
+         }
+     }
+
+     String commonPrefix(String left, String right){
+         int min = Math.min(left.length(), right.length());
+         for(int i=0; i<min; i++){
+             if(left.charAt(i) != right.charAt(i)){
+                 return left.substring(0, i);
+             }
+         }
+         return left.substring(0, min);
+     }
 }
