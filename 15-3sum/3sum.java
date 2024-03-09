b5678866a5248d28f7fad5df35300b264c94fb61
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2024-03-09 15:13:13 -0600

    Time: 30 ms (75.86%) | Memory: 51.2 MB (78.88%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
         Arrays.sort(nums);
-        for(int i=0; i<n-2; i++){
+        for(int i=0; i<nums.length-2; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i+1;
-            int right = n-1;
+            int right = nums.length-1;
             while(left < right){
-                int currentSum = nums[i] + nums[left] + nums[right];
-                if(currentSum == 0){
+                int currSum = nums[i] + nums[left] + nums[right];
+                if(currSum == 0){
                     List<Integer> list = Arrays.asList(nums[i], nums[left], nums[right]);
                     result.add(list);
                     right--;
                     left++;
-                    while(left < right && nums[left] == nums[left-1]){
+                    while(left<right && nums[left] == nums[left-1]){
                         left++;
                     }
-                    while(left < right && nums[right] == nums[right+1]){
+                    while(left<right && nums[right] == nums[right+1]){
                         right--;
                     }
-                }else if(currentSum > 0){
+                }else if(currSum > 0){
                     right--;
-                } else{
+                }else{
                     left++;
                 }
-                }
             }
-            return result;
         }
+        return result;
+        
+    }
 }
