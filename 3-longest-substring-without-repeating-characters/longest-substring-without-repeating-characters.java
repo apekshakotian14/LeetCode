5a5500a16278894cfad7e9a688c1560257ff5d61
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2024-04-09 11:53:42 -0500

    Time: 5 ms (87.37%) | Memory: 44.5 MB (52.49%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,8 +1,9 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
-        int slow = 0;
+
         int max = 0;
+        int slow = 0;
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             if(map.containsKey(c)){
@@ -11,6 +12,7 @@ class Solution {
             map.put(c, i);
             max = Math.max(max, i-slow+1);
         }
+
         return max;
     }
 }
