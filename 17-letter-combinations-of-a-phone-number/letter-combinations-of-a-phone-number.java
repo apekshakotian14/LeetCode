ca484b453c151a3905e5be142a279218b81881c6
apekshakotian14 <127633105+apekshakotian14@users.noreply.github.com>
2024-04-17 18:05:20 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (51.05%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -4,27 +4,27 @@ class Solution {
         '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
         '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
     private String phoneDigits;
-    
+
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0) {
+        if(digits.length() == 0){
             return combinations;
         }
-        
+
         phoneDigits = digits;
         backtrack(0, new StringBuilder());
         return combinations;
     }
-    
-    private void backtrack(int index, StringBuilder path) {
-        if (path.length() == phoneDigits.length()) {
+
+    private void backtrack(int index, StringBuilder path){
+        if(path.length() == phoneDigits.length()){
             combinations.add(path.toString());
-            return; 
+            return;
         }
 
         String possibleLetters = letters.get(phoneDigits.charAt(index));
-        for (char letter: possibleLetters.toCharArray()) {
+        for(char letter: possibleLetters.toCharArray()){
             path.append(letter);
-            backtrack(index + 1, path);
+            backtrack(index+1, path);
             path.deleteCharAt(path.length() - 1);
         }
     }
